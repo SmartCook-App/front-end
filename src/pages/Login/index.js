43d8b229efe50b1d544055d2f2ad76ce96c84fff
unknown <fivaldivia@uc.cl>
2021-06-29 10:58:16 -0400

    Avances con formulario y botón

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,29 +1,54 @@
 import React, { useState, useContext } from 'react';
-import { Button, Text, View, TextInput } from 'react-native';
+import { Button, Text, View, TextInput,  TouchableOpacity, Form } from 'react-native';
 import AuthContext from '../../config';
+import logInRegisterStyle from '../../styles/logInRegister';
 
 export default function Login({ navigation }) {
 
-  const [username, setUsername] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [lastName, setlastName] = React.useState('');
+  const [email, setemail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   const { state, dispatch } = useContext(AuthContext);
 
   return (
-    <View>
+    <View style={logInRegisterStyle.container}>
       <Text>Page Login</Text>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' })} />
+        <TextInput
+          placeholderTextColor='white'
+          style={logInRegisterStyle.textInput}
+          placeholder="Nombre"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          placeholderTextColor='white'
+          style={logInRegisterStyle.textInput}
+          placeholder="Apellido"
+          value={lastName}
+          onChangeText={setlastName}
+        />
+        <TextInput
+          placeholderTextColor='white'
+          style={logInRegisterStyle.textInput}
+          placeholder="Correo"
+          value={email}
+          onChangeText={setemail}
+        />
+        <TextInput
+          placeholderTextColor='white'
+          style={logInRegisterStyle.textInput}
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity title="Sign in" onPress={() => alert('Hello, world!')} style={logInRegisterStyle.button}>
+          <Text style={logInRegisterStyle.buttonText}>Registrarse</Text>
+        </TouchableOpacity>
+        <Text>Al crear una cuenta estas aceptando los Términos del servicio y Política de privacidad</Text>
+      {/* onPress={() => dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' })} */}
     </View>
   );
 }
