14be9f93aa8858213c26024711d5172a01027d6f
unknown <fivaldivia@uc.cl>
2021-08-01 19:52:00 -0400

    Avance en slide de home

--- components/LoginRegisterComponents/SlideComponent.tsx
+++ components/LoginRegisterComponents/SlideComponent.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { Animated, View, Text, StyleSheet, FlatList, useWindowDimensions } from "react-native";
 import slide from './Slide';
 import SlideComponentItem from './SlideComponentItem';
 
+
 export default function SlideComponent() {
+    const { width } = useWindowDimensions();
+    const scrollX = new Animated.Value(250)
+    let position = Animated.divide(scrollX, width)
+
     return (
         <View style={styles.container}>
             <FlatList
                 data={slide}
                 renderItem={({ item }) => < SlideComponentItem item={item} />}
                 horizontal
-                showsHorizontalScrollIndicator={true}
+                showsHorizontalScrollIndicator={false}
                 pagingEnabled
+                scrollEnabled
+                snapToAlignment = 'center'
+                scrollEventThrottle = {16}
+                decelerationRate = {"fast"}
                 bounces={false}
                 keyExtractor={(item) => item.id}
+                onScroll = {Animated.event(
+                    [null, { nativeEvent: { contentOffset: { x: scrollX } } }]
+                )}
             />
+            <View style={styles.dotView}>
+                {slide.map((_, i) => {
+                    let opacity = position.interpolate({
+                        inputRange: [i-1, i, i+1],
+                        outputRange: [0.3, 1, 0.3],
+                        extrapolate: 'clamp'
+                    })
+                    return(
+                        <Animated.View
+                            key = {i}
+                            style = {{opacity, height: 10, width:10, backgroundColor:'#595959', }}
+                        />
+                    )
+                })}
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    dotView: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
