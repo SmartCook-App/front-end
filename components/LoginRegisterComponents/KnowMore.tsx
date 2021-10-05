0a6539ee6d74f92520a68a6ddd41a44489b1837c
Santiago Matus <santiagomatus@MacBook-Air-de-Santiago.local>
2021-10-04 23:51:54 -0300

    first entry

---
+++ components/LoginRegisterComponents/KnowMore.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { Text, View } from "react-native";
+import FiltersComponentsStyle from "../../styles/HomeComponentsStyle/FiltersComponentsStyle";
+import Carousel from "react-native-snap-carousel";
+
+interface Props {
+  id: any;
+}
+
+const KnowMore: FC<Props> = (props: Props) => {
+
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+export default KnowMore;
