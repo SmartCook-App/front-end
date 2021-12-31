0fbd089d6785b37a5ec9572400f0eb3cfa958fa5
Mjtala <mjtala@uc.cl>
2021-12-31 18:32:55 -0300

    feat: ahora esta default login y no registrar

--- assets/Languages/ShoppingListScreenLanguages.ts
+++ assets/Languages/ShoppingListScreenLanguages.ts
@@ -2,13 +2,13 @@ const SLSL: any = {
   ES: {
     units: "un",
     searchbarPlaceholder: "Buscar ingredientes",
-    searchbarRecipiePlaceholder: "Buscar recetas",
+    searchbarRecipiePlaceholder: "Buscar recetas/cocineros",
     finishButtonLabel: "Marcar lista como completada",
   },
   EN: {
     units: "un",
     searchbarPlaceholder: "Search ingredients",
-    searchbarRecipiePlaceholder: "Search recipies",
+    searchbarRecipiePlaceholder: "Search recipies/cookers",
     finishButtonLabel: "Finish list",
   },
 };
