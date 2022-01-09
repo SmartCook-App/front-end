4bcc0d484c84048cb9d6e971e3f53d8d2a8959e8
fivaldivia <fivaldivia@uc.cl>
2022-01-09 13:35:00 -0300

    fix(styles): changed size of calendar in CalendarScreen

--- screens/Calendar/CalendarIndexScreen.tsx/CalendarStyles.ts
+++ screens/Calendar/CalendarIndexScreen.tsx/CalendarStyles.ts
@@ -3,29 +3,29 @@ import { StyleSheet } from 'react-native';
 import Colors from '../../../assets/Colors';
 
 const CalendarStyle = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.light.background,
-      fontFamily: 'nunito-regular',
-    },
-    calendarContainer: {
-      flex: 0.8,
-      borderBottomWidth: normalizePx(1),
-      borderBottomColor: Colors.light.greyOfFilters,
-    },
-    calendarText: {
-      fontFamily: 'nunito-semiBold',
-      color: Colors.light.text,
-    },
-    selectedDateText: {
-      fontSize: normalizeFontSize(17),
-      fontFamily: 'nunito-semiBold',
-      marginVertical: normalizePx(10),
-    },
-    savedRecipes: {
-      flex: 1,
-      marginHorizontal: normalizePx(10),
-    },
-  });
-  
-  export default CalendarStyle;
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    fontFamily: 'nunito-regular',
+  },
+  calendarContainer: {
+    flex: 1,
+    borderBottomWidth: normalizePx(1),
+    borderBottomColor: Colors.light.greyOfFilters,
+  },
+  calendarText: {
+    fontFamily: 'nunito-semiBold',
+    color: Colors.light.text,
+  },
+  selectedDateText: {
+    fontSize: normalizeFontSize(17),
+    fontFamily: 'nunito-semiBold',
+    marginVertical: normalizePx(10),
+  },
+  savedRecipes: {
+    flex: 1,
+    marginHorizontal: normalizePx(10),
+  },
+});
+
+export default CalendarStyle;
